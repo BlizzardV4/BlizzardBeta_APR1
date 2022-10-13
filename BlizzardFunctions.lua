87c1b85fe7824e93e237ba0e9226e7cf1f2a7a43
BlizzardV4 <114962099+BlizzardV4@users.noreply.github.com>
2022-10-13 14:49:57 -0700

    Update BlizzardFunctions.lua

--- BlizzardFunctions.lua
+++ BlizzardFunctions.lua
@@ -1,1 +1,12 @@
-
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Camera = workspace.Camera
+local RunService = game:GetService("RunService")
+local UserInput = game:GetService("UserInputService")
+local Tween = game:GetService("TweenService")
+local Holding = false
+function Aimbot()
+	while _G.Aimbot == true do
+      print("enabled")
+	end
+end
